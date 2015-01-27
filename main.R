3690f6b44930cafb78e92597d75da4cb93f2c690
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2015-01-27 09:05:26 +0200

    added more used library

--- main.R
+++ main.R
@@ -1,7 +1,11 @@
-library("foreign")
+library(foreign)
+library(plyr)
+library(reshape)
+library(tidyr)
 
 # read sample data file to polish the data read & filter
 data <- read.dta("cepr_org_2013.dta")
 
 # age-based filter
 data <- subset(data, age > 25 & age < 55)
+
