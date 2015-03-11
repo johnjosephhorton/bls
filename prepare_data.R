72b123c708199e4977b2403a9dbeb1ffad5ac509
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2015-03-11 13:36:10 +0200

    R script listing all data zip archives

---
+++ prepare_data.R
@@ -0,0 +1,8 @@
+library("rvest")
+
+data_url <- "http://ceprdata.org/cps-uniform-data-extracts/cps-outgoing-rotation-group/cps-org-data/"
+html <- html(data_url)
+
+zips <- html %>% html_nodes(xpath = "//div[@class='content']/ul/li/a") %>% html_attr(name="href")
+
+print(zips)
