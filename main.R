83d7982ef6b49b7a3adbd8965eb11de66a05ed31
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2015-05-28 14:13:14 +0300

    dplyr solution for cleaning and summarizing

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 library(data.table)
 library(dplyr)
 library(reshape2)
-library(tidyr)
+library(ggplot2)
 
 options(na.action = na.omit)
 options(digits = 4)
@@ -9,28 +9,15 @@ options(digits = 4)
 # load necessary functions
 source("helpers.R")
 
+if(!exists("data2012")) {
+  data2012 <- fread("data/cepr_org_2012.csv", colClasses=c("hhid"="character", "hhid2"="character"))
+}
+
 if(!exists("data2013")) {
   data2013 <- fread("data/cepr_org_2013.csv", colClasses=c("hhid"="character", "hhid2"="character"))
 }
 
-data2012 <- fread("data/cepr_org_2012.csv", colClasses=c("hhid"="character", "hhid2"="character"))
-data2013 <- fread("data/cepr_org_2013.csv", colClasses=c("hhid"="character", "hhid2"="character"))
-
-obs.total <- length(data2013$age)
-# length(data2013$empl[data2012$empl])
-
-# empl, unem, selfemp, selfinc
-
-print(c("Age:", field.summary.v2(data2013, "age", NA, function(x) { return(mean(x, na.rm=T)) }, c)))
-
-print("Races:")
-print(c("White: ", field.summary.v2(data2013, "wbho", "White", length, total.part)))
-print(c("Hispanic: ", field.summary.v2(data2013, "wbho", "Hispanic", length, total.part)))
-print(c("Black: ", field.summary.v2(data2013, "wbho", "Black", length, total.part)))
-
-print("Degree")
-print(c("Advanced: ", field.summary.v2(data2013, "educ", "Advanced", length, total.part)))
-print(c("College: ", field.summary.v2(data2013, "educ", "College", length, total.part)))
-print(c("HS: ", field.summary.v2(data2013, "educ", "HS", length, total.part)))
-print(c("LTHS: ", field.summary.v2(data2013, "educ", "LTHS", length, total.part)))
-print(c("Some college: ", field.summary.v2(data2013, "educ", "Some college", length, total.part)))
+# summarise of empl, unem, selfemp, selfinc based on educ (Education) and wbho (Race)
+educ.wbho.summ <- group_by(data2012, wbho, educ) %>%
+  summarise(mean.age=mean(age), empl=100*sum(empl, na.rm=T)/n(), selfemp=100*sum(selfemp, na.rm=T)/n(), selfinc=100*sum(selfinc, na.rm=T)/n(), unem=100*sum(unem, na.rm=T)/n(), earn) %>%
+  arrange(wbho, educ)
