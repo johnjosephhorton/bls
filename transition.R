a9d3627f1a6ac5d9f6b17e7520570e84668d60e1
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2015-06-13 11:38:08 +0300

    various fixes

--- transition.R
+++ transition.R
@@ -1,20 +1,24 @@
 library(data.table)
 library(reshape2)
-library(stringr)
+library(dplyr)
 
 data2012 <- fread("data/cepr_org_2012.csv", colClasses=c("hhid"="character", "hhid2"="character"))
 data2013 <- fread("data/cepr_org_2013.csv", colClasses=c("hhid"="character", "hhid2"="character"))
 
-sub.data2013 <- data2013[, .(year, month, hhid, hhid2, age, lfstat, empl, unem, nilf, selfemp, selfinc)]
-sub.data2012 <- data2012[, .(year, month, hhid, hhid2, age, lfstat, empl, unem, nilf, selfemp, selfinc)]
+sub.data2013 <- data2013[, .(year, hhid, hhid2, age, lfstat, empl, unem, nilf, selfemp, selfinc)]
+sub.data2012 <- data2012[, .(year, hhid, hhid2, age, lfstat, empl, unem, nilf, selfemp, selfinc)]
 
-sub.data2012$hh <- with(sub.data2012, paste0(hhid, hhid2, ".", year, ".", age))
-sub.data2013$hh <- with(sub.data2013, paste0(hhid, hhid2, ".", year, ".", age))
+sub.data2012$hh <- with(sub.data2012, paste0(hhid, hhid2, ".", age))
+sub.data2013$hh <- with(sub.data2013, paste0(hhid, hhid2, ".", age-1))
 
 merged.data <- rbind(sub.data2012, sub.data2013)
 merged.data <- merged.data[complete.cases(merged.data), ]
 
-merged.data <- merged.data[order(merged.data$hh), ]
+merged.data <- merged.data[duplicated(merged.data$hh) | duplicated(merged.data$hh, fromLast = T), ]
+
+compare.work <- function(prev, curr) {
+  switch()
+}
 
 res <- data.frame(t(apply(merged.data[-1], 1, function(x) names(merged.data[-1])[x != 0])))
 dcast(res, X1 ~ X2)
