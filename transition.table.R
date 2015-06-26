09be83be28be7ac85b3114535384bdf29b41fde6
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2015-06-26 13:23:09 +0300

    employement transition calculations #7

--- transition.table.R
+++ transition.table.R
@@ -2,21 +2,25 @@ empl.empl <- 0
 empl.unem <- 0
 empl.selfemp <- 0
 empl.selfinc <- 0
+empl.total <- 0
 
 unem.empl <- 0
 unem.unem <- 0
 unem.selfemp <- 0
 unem.selfinc <- 0
+unem.total <- 0
 
 selfemp.empl <- 0
 selfemp.unem <- 0
 selfemp.selfemp <- 0
 selfemp.selfinc <- 0
+selfemp.total <- 0
 
 selfinc.empl <- 0
 selfinc.unem <- 0
 selfinc.selfemp <- 0
 selfinc.selfinc <- 0
+selfinc.total <- 0
 
 total <- 0
 
@@ -25,16 +29,19 @@ transit <- function(from, to) {
     name <- paste(from, to, sep = ".")
     val <- get(name, envir = globalenv())
     assign(name, val + 1, envir = globalenv())
+    
+    total <- paste(from, "total", sep = ".")
+    val <- get(total, envir = globalenv())
+    assign(total, val + 1, envir = globalenv())
   }
 }
 
 library(utils)
 
-pb <- txtProgressBar(0, nrow(merged.data), 0, style = 3)
+hh2012 <- merged.data[year == 2012, hh]
+pb <- txtProgressBar(0, length(hh2012), 0, style = 3)
 
-
-
-system.time(for(i in merged.data$hh) {
+system.time(for(i in hh2012) {
   sub <- merged.data[hh == i, ]
   
   setTxtProgressBar(pb, total)
@@ -78,6 +85,7 @@ transition <- matrix(c(empl.empl, empl.unem, empl.selfemp, empl.selfinc,
                 nrow = 4,
                 ncol = 4)
 
-start.total <- unlist(lapply(c("empl", "unem", "selfemp", "selfinc"), function(x) nrow(merged.data[year == 2012 & merged.data[x] == 1, ])))
+start.total <- unlist(lapply(c("empl", "unem", "selfemp", "selfinc"), function(x) nrow(merged.data[year == 2012 & merged.data[[x]] == 1, ])))
+start.total <- c(empl.total, unem.total, selfemp.total, selfinc.total)
 
-t(apply(transition, 1, function(x) x/start.total))
+apply(t(transition), 1, "/", start.total)
